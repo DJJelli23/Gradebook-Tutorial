e69d50184b32aebeabe03624b7500d8a249497fd
DJ Jellings <dj_jellings@yahoo.com>
2020-12-08 12:10:38 -0700

    Finishing Chapter 3
    Finished - Using a List

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,15 +8,17 @@ namespace GradeBook
         static void Main(string[] args)
         {
             double[] numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
-            List<double> grades = new List<double>();
+            List<double> grades = new List<double>(){ 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
             // numbers[0] = 12.7;
             // numbers[1] = 10.5;
             // numbers[2] = 16.8;
             var result = 0.0;
-            foreach (double number in numbers)
+            foreach (double number in grades)
             {
                 result += number;
             }
+            var average = result / grades.Count;
             // var result = numbers[0];
             // result += numbers[1];
             // result += numbers[2];
@@ -26,6 +28,7 @@ namespace GradeBook
             // var result = x + y;
 
             Console.WriteLine(result);
+            Console.WriteLine(average);
 
             if(args.Length > 0)
             {
