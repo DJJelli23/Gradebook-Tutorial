5a89faeb654d95e75f56f2b4f895476bbba85b2e
DJ Jellings <dj_jellings@yahoo.com>
2020-12-08 12:18:34 -0700

    Cleanup
    Removed unneeded comments.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,24 +10,16 @@ namespace GradeBook
             double[] numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
             List<double> grades = new List<double>(){ 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
-            // numbers[0] = 12.7;
-            // numbers[1] = 10.5;
-            // numbers[2] = 16.8;
             var result = 0.0;
             foreach (double number in grades)
             {
                 result += number;
             }
             result /= grades.Count;
-            // var result = numbers[0];
-            // result += numbers[1];
-            // result += numbers[2];
-            // double x = 34.1;
-            // double y = 10.3;
 
-            // var result = x + y;
 
-            Console.WriteLine($"The average grade is {result:N1}!");// N1 is the amount of decimals spaces after the point.
+            // N1 is the amount of decimals spaces after the point.
+            Console.WriteLine($"The average grade is {result:N1}!");
 
             if(args.Length > 0)
             {
