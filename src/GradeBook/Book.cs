be29f4919931b8b2c421b0a0a43aec4038b901de
DJ Jellings <dj_jellings@yahoo.com>
2020-12-08 12:43:07 -0700

    Update Chapter 4
    Working on the Book class and created a contructor which finalizes the initialation of the grades List.

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     class Book
     {
+        List<double> grades;
 
+        public Book()
+        {
+            grades = new List<double>();   
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,6 +8,7 @@ namespace GradeBook
         static void Main(string[] args)
         {
             Book book = new Book();
+            book.AddGrade(89.1);
 
             List<double> grades = new List<double>(){ 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
