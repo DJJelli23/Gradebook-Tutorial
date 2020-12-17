14f9fc345431caa699d75117522d34b0d5d30654
DJ Jellings <dj_jellings@yahoo.com>
2020-12-17 11:17:30 -0700

    Adding Parameters to Book Contructor
    Added the name parameter to the Book class contructor.

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -4,11 +4,13 @@ namespace GradeBook
 {
     class Book
     {
-        List<double> grades;
+        private List<double> grades;
+        private string name;
 
-        public Book()
+        public Book(string name)
         {
-            grades = new List<double>();   
+            grades = new List<double>(); 
+            this.name = name;  
         }
         public void AddGrade(double grade)
         {
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,12 +7,14 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Book book = new Book();
+            Book book = new Book("Scott's Grade Book");
             book.AddGrade(89.1);
+            book.AddGrade(90.5);
 
             List<double> grades = new List<double>(){ 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
 
+            // Below we are computing an average.
             var result = 0.0;
             foreach (double number in grades)
             {
