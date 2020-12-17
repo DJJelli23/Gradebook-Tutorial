faf15ead2a9b29b0f545c82647c7e2d7e187beb0
DJ Jellings <dj_jellings@yahoo.com>
2020-12-17 11:50:37 -0700

    Finished Encapsulation Challenge
    Moved all the processing numbers code into the Book class to calculate the average, lowest, and highest values. It also displays from there.

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -15,6 +16,28 @@ namespace GradeBook
         public void AddGrade(double grade)
         {
             grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            // Below we are computing an average.
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (double number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            result /= grades.Count;
+
+
+            List<double> stats = new List<double>(){result, highGrade,lowGrade};
+            // N1 is the amount of decimals spaces after the point.
+            Console.WriteLine($"The average grade is {result:N1}!");
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}!");
+            Console.WriteLine($"The highest grade is {highGrade:N1}!");
         }
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -11,26 +11,7 @@ namespace GradeBook
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
-
-            List<double> grades = new List<double>(){ 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
-
-            // Below we are computing an average.
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-            foreach (double number in grades)
-            {
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade);
-                result += number;
-            }
-            result /= grades.Count;
-
-            // N1 is the amount of decimals spaces after the point.
-            Console.WriteLine($"The average grade is {result:N1}!");
-            Console.WriteLine($"The lowest grade is {lowGrade:N1}!");
-            Console.WriteLine($"The highest grade is {highGrade:N1}!");
+            book.ShowStatistics();
         }
     }
 }
